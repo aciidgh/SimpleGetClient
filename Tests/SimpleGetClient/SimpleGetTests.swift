86c8339e1c0fcc68f55bb56e7c457da9e54e3c39
Ankit Aggarwal <ankit.spd@gmail.com>
2016-02-17 17:25:49 +0530

    Add different method for OSX

--- Tests/SimpleGetClient/SimpleGetTests.swift
+++ Tests/SimpleGetClient/SimpleGetTests.swift
@@ -6,6 +6,7 @@ class SimpleGetTests: XCTestCase {
 
     let client = GetClient()
 
+#if os(Linux)
     func testGetRequestWithOneArg() {
         let result = client.fetch("http://httpbin.org/get?a=b")
         guard case let json as Dictionary<String, Any> = try? NSJSONSerialization.JSONObjectWithData(NSString(string: result.response).dataUsingEncoding(NSUTF8StringEncoding)!, options: NSJSONReadingOptions.AllowFragments) else {
@@ -20,6 +21,17 @@ class SimpleGetTests: XCTestCase {
         
         XCTAssertEqual(argA, "b", "Incorrect value received from server")
     }
+#else 
+func testGetRequestWithOneArg() {   
+    let result = client.fetch("http://httpbin.org/get?a=b")
+    guard case let json as NSDictionary = try? NSJSONSerialization.JSONObjectWithData((result.response as NSString).dataUsingEncoding(NSUTF8StringEncoding)!, options: NSJSONReadingOptions.AllowFragments) else {
+        XCTFail("Couldn't parse data")
+        return
+    }
+    
+    XCTAssertEqual(json["args"]?["a"], "b", "Incorrect value received from server")
+}
+#endif
     
     func testGetRequestStatusCode() {
         let result = client.fetch("http://httpbin.org/status/419")

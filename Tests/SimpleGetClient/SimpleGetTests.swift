f8609662cdfe720877594dbcb3df6acaa457a84c
Ankit Agarwal <ankit.spd@gmail.com>
2016-02-17 17:20:20 +0530

    Add Linux Test support

--- Tests/SimpleGetClient/SimpleGetTests.swift
+++ Tests/SimpleGetClient/SimpleGetTests.swift
@@ -1,19 +1,24 @@
 @testable import SimpleGetClient
 import XCTest
+import Foundation
 
 class SimpleGetTests: XCTestCase {
 
     let client = GetClient()
 
-    func testGetRequestWithOneArg() {   
-        
+    func testGetRequestWithOneArg() {
         let result = client.fetch("http://httpbin.org/get?a=b")
-        guard case let json as NSDictionary = try? NSJSONSerialization.JSONObjectWithData((result.response as NSString).dataUsingEncoding(NSUTF8StringEncoding)!, options: NSJSONReadingOptions.AllowFragments) else {
+        guard case let json as Dictionary<String, Any> = try? NSJSONSerialization.JSONObjectWithData(NSString(string: result.response).dataUsingEncoding(NSUTF8StringEncoding)!, options: NSJSONReadingOptions.AllowFragments) else {
             XCTFail("Couldn't parse data")
             return
         }
+        guard case let args as Dictionary<String, Any> = json["args"],
+                  case let argA as String = args["a"]  else {
+            XCTFail("Couldn't parse data")
+            return
+        } 
         
-        XCTAssertEqual(json["args"]?["a"], "b", "Incorrect value received from server")
+        XCTAssertEqual(argA, "b", "Incorrect value received from server")
     }
     
     func testGetRequestStatusCode() {
@@ -21,3 +26,14 @@ class SimpleGetTests: XCTestCase {
         XCTAssertEqual(result.responseCode, "419", "Incorrect value received from server")
     }
 }
+
+#if os(Linux)
+extension SimpleGetTests: XCTestCaseProvider {
+    var allTests : [(String, () throws -> Void)] {
+        return [
+            ("testGetRequestWithOneArg", testGetRequestWithOneArg),
+            ("testGetRequestStatusCode", testGetRequestStatusCode),
+        ]
+    }
+}
+#endif
